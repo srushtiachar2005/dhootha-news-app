d4cd9d260a5b1e78b38cfe56069660ac8e793b9f
J Srushti <srushtiachar61@gmail.com>
2025-06-26 18:19:33 +0530

    Update news_client.py

--- news_client.py
+++ news_client.py
@@ -1,6 +1,4 @@
-# news_client.py
 import os
-import streamlit as st
 import requests
 from dotenv import load_dotenv
 
@@ -8,9 +6,15 @@ load_dotenv()
 
 class NewsAPIClient:
     def __init__(self, api_key=None):
-        self.api_key = api_key or os.getenv("NEWSAPI_KEY") or st.secrets.get("NEWSAPI_KEY")
+        try:
+            import streamlit as st
+            self.api_key = api_key or st.secrets.get("NEWSAPI_KEY")
+        except:
+            self.api_key = api_key or os.getenv("NEWSAPI_KEY")
+
         if not self.api_key:
             raise ValueError("API key is missing.")
+
         self.base_url = "https://newsapi.org/v2"
 
     def get_top_headlines(self, **params):
